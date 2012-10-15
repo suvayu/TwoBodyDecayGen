97519cd4ee9955f51eff098419f64daa929cdb27
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2012-10-15 20:28:14 +0200

    Add commandline parameter for # of events

--- main.cc
+++ main.cc
@@ -1,14 +1,25 @@
 #include <iostream>
+#include <cstdlib>
 
 #include <TFile.h>
 #include "TwoBodyDecayGen.hxx"
 
 
-int main()
+int main(int argc, char* argv[])
 {
+  if (argc > 2) {
+    std::cout << "Too many arguments!" << std::endl;
+    return -1;
+  }
+
+  unsigned long nevents(100);
+  if (argc == 2) {
+    nevents = atol(argv[1]);
+  }
+
   double masses[NDAUS] = {1.968, 0.494};
   TwoBodyDecayGen generator(masses);
-  TTree* eventtree = generator.get_event_tree(100);
+  TTree* eventtree = generator.get_event_tree(nevents);
   std::cout << eventtree->GetEntries() << " events generated" << std::endl;
 
   // eventtree->Scan("evt_wt");
