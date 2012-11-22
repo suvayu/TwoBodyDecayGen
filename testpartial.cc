b25995a707f2f5ae88dd7129ab430cff78e321cd
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2012-11-22 02:45:26 +0100

    Cleanup tests for Ds*π

--- testpartial.cc
+++ testpartial.cc
@@ -19,7 +19,7 @@ static const double BSMASS(5366.3), DSMASS(1968.49), KMASS(493.677),
   /*, RHOMASS(775.49), DSTMASS(2010.25), DSTMASS2(2460.1);*/
 
 
-int kfactorp(TTree *outtree);
+int kfactorp(TTree *outtree, std::string mode);
 
 
 int main(int argc, char* argv[])
@@ -44,13 +44,12 @@ int main(int argc, char* argv[])
 
   // B momentum distribution from MC
   TH1D Bsmomp("Bsmomp", "", 100, 0.0, 300.0);
-  TH1D dau1momp("dau1momp", "", 100, 0.0, 300.0);
-  TH1D dau2momp("dau2momp", "", 100, 0.0, 300.0);
 
+  fname = mode + "_Bs_mom_MC.png";
   intree->Draw("1E-3*BsMom.P()>>Bsmomp");
   label->DrawLatex(0.6, 0.5, "B_{s} momentum (MC)");
   gPad->Update();
-  gPad->Print("Bs_mom_MC.png");
+  gPad->Print(fname.c_str());
 
 
   // read generated 4-vectors
@@ -58,14 +57,15 @@ int main(int argc, char* argv[])
   TFile outfile(fname.c_str(), "read");
   TTree *outtree = dynamic_cast<TTree*>(outfile.Get("TwoBodyDecayGen_decaytree"));
 
-  kfactorp(outtree);
+  kfactorp(outtree, mode);
 
   return 0;
 }
 
 
-int kfactorp(TTree *outtree)
+int kfactorp(TTree *outtree, std::string mode)
 {
+  std::string fname;
   std::vector<TLorentzVector> *particle_lvs = NULL;
   TLorentzVector BsMom(0.0, 0.0, 0.0, 0.0);
   TLorentzVector DsMom(0.0, 0.0, 0.0, 0.0);
@@ -76,7 +76,9 @@ int kfactorp(TTree *outtree)
 
   long oentries(outtree->GetEntries());
 
-  TH1D kfactor("kfactor", "", 100, 0., 1.15);
+  TH1D hkfactorm("hkfactorm", "", 100, 0., 1.15);
+  TH1D hkfactorp("hkfactorp", "", 100, 0., 1.15);
+  TH1D hkfactorpm("hkfactorpm", "", 100, 0., 1.15);
 
   double kfactorp(0.0), kfactorm(0.0), kfactorpm(0.0);
 
@@ -90,20 +92,42 @@ int kfactorp(TTree *outtree)
     kfactorm = (*particle_lvs)[0].M() / Bs_rec.M();
     kfactorpm = kfactorp * kfactorm;
 
-    kfactor.Fill(kfactorpm);
+    hkfactorm.Fill(kfactorm);
+    hkfactorp.Fill(kfactorp);
+    hkfactorpm.Fill(kfactorpm);
   }
 
-  kfactor.SetLineColor(kAzure);
-  kfactor.Draw("hist");
+  hkfactorm.SetLineColor(kAzure);
+  hkfactorp.SetLineColor(kAzure);
+  hkfactorpm.SetLineColor(kAzure);
 
   TLatex *label = new TLatex();
-  label->DrawLatex(0.6, 0.5, "k-factor (gen)");
+  label->SetTextSize(0.04);
+  label->SetNDC(true);
 
+  hkfactorm.Draw("hist");
+  label->DrawLatex(0.4, 0.5, "k-factor (m)");
+  fname = mode + "_kfactorm_gen.png";
   gPad->Update();
-  gPad->Print("kfactor_DsstPi_gen.png");
+  gPad->Print(fname.c_str());
 
-  TFile hdump("hdump-DsstPi.root", "update");
-  hdump.WriteTObject(&kfactor, NULL, "WriteDelete");
+  hkfactorp.Draw("hist");
+  label->DrawLatex(0.4, 0.5, "k-factor (p)");
+  fname = mode + "_kfactorp_gen.png";
+  gPad->Update();
+  gPad->Print(fname.c_str());
+
+  hkfactorpm.Draw("hist");
+  label->DrawLatex(0.4, 0.5, "k-factor (m/p)");
+  fname = mode + "_kfactorpm_gen.png";
+  gPad->Update();
+  gPad->Print(fname.c_str());
+
+  fname = "hdump-" + mode + ".root";
+  TFile hdump(fname.c_str(), "update");
+  hdump.WriteTObject(&hkfactorm, NULL, "WriteDelete");
+  hdump.WriteTObject(&hkfactorp, NULL, "WriteDelete");
+  hdump.WriteTObject(&hkfactorpm, NULL, "WriteDelete");
   hdump.Close();
 
   return 0;
