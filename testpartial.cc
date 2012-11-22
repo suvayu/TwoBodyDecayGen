42026d977a01ab9c46c39d61bd762472c9059593
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2012-11-22 17:28:13 +0100

    Overlay MC and generated k-factor distributions
    MC distributions for the mass k-factor is missing.  Inconvenient to
    get it, take a look later.

--- testpartial.cc
+++ testpartial.cc
@@ -10,6 +10,7 @@
 #include <TLatex.h>
 #include <TLorentzVector.h>
 #include <TLegend.h>
+#include <TList.h>
 
 
 // some constants
@@ -19,7 +20,7 @@ static const double BSMASS(5366.3), DSMASS(1968.49), KMASS(493.677),
   /*, RHOMASS(775.49), DSTMASS(2010.25), DSTMASS2(2460.1);*/
 
 
-int kfactorp(TTree *outtree, std::string mode, std::string fext);
+int kfactorp(TTree *outtree, TTree *MCtree, std::string mode, std::string fext);
 
 
 int main(int argc, char* argv[])
@@ -62,28 +63,43 @@ int main(int argc, char* argv[])
   TFile outfile(fname.c_str(), "read");
   TTree *outtree = dynamic_cast<TTree*>(outfile.Get("TwoBodyDecayGen_decaytree"));
 
-  kfactorp(outtree, mode, fext);
+  fname = "treedump.root";
+  TFile MCfile(fname.c_str(), "update");
+  TTree *MCtree1 = dynamic_cast<TTree*>(MCfile.Get("mBresn_Bs2DsstPi_down"));
+  TTree *MCtree2 = dynamic_cast<TTree*>(MCfile.Get("mBresn_Bs2DsstPi_up"));
+
+  TList *tlist = new TList();
+  tlist->Add(MCtree1);
+  tlist->Add(MCtree2);
+  TTree *MCtree = TTree::MergeTrees(tlist);
+
+  kfactorp(outtree, MCtree, mode, fext);
 
   return 0;
 }
 
 
-int kfactorp(TTree *outtree, std::string mode, std::string fext)
+int kfactorp(TTree *outtree, TTree *MCtree, std::string mode, std::string fext)
 {
   std::string fname;
+
+  // generated tree
   std::vector<TLorentzVector> *particle_lvs = NULL;
   TLorentzVector BsMom(0.0, 0.0, 0.0, 0.0);
   TLorentzVector DsMom(0.0, 0.0, 0.0, 0.0);
   TLorentzVector hMom(0.0, 0.0, 0.0, 0.0);
   TLorentzVector Bs_rec(0.0, 0.0, 0.0, 0.0);
-
   outtree->SetBranchAddress("particle_lvs", &particle_lvs);
 
   long oentries(outtree->GetEntries());
 
-  TH1D hkfactorm("hkfactorm", "", 100, 0., 1.15);
-  TH1D hkfactorp("hkfactorp", "", 100, 0., 1.15);
-  TH1D hkfactorpm("hkfactorpm", "", 100, 0., 1.15);
+  TH1D hkfactorm("hkfactorm", "", 100, 0.4, 1.2);
+  TH1D hkfactorp("hkfactorp", "", 100, 0.4, 1.2);
+  TH1D hkfactorpm("hkfactorpm", "", 100, 0.4, 1.2);
+
+  hkfactorm.SetLineColor(kRed);
+  hkfactorp.SetLineColor(kRed);
+  hkfactorpm.SetLineColor(kRed);
 
   double kfactorp(0.0), kfactorm(0.0), kfactorpm(0.0);
 
@@ -102,29 +118,54 @@ int kfactorp(TTree *outtree, std::string mode, std::string fext)
     hkfactorpm.Fill(kfactorpm);
   }
 
-  hkfactorm.SetLineColor(kAzure);
-  hkfactorp.SetLineColor(kAzure);
-  hkfactorpm.SetLineColor(kAzure);
+  // dumped tree
+  MCtree->SetBranchAddress("kfactor", &kfactorpm);
+  MCtree->SetBranchAddress("kfactorp", &kfactorp);
 
-  TLatex *label = new TLatex();
-  label->SetTextSize(0.04);
-  label->SetNDC(true);
+  oentries = MCtree->GetEntries();
+
+  TH1D hMCkfactorp("hMCkfactorp", "", 100, 0.4, 1.2);
+  TH1D hMCkfactorpm("hMCkfactorpm", "", 100, 0.4, 1.2);
+
+  hMCkfactorp.SetLineColor(kAzure);
+  hMCkfactorpm.SetLineColor(kAzure);
+
+  for (unsigned i = 0; i < oentries; ++i) {
+    MCtree->GetEntry(i);
+    hMCkfactorp.Fill(kfactorp);
+    hMCkfactorpm.Fill(kfactorpm);
+  }
+
+  // TLatex *label = new TLatex();
+  // label->SetTextSize(0.04);
+  // label->SetNDC(true);
+
+  TLegend legend(0.2, 0.6, 0.45, 0.4);
+  legend.AddEntry(&hMCkfactorpm, "Monte Carlo", "l");
+  legend.AddEntry(&hkfactorpm, "Generated", "l");
+  legend.SetFillColor(0);
+  legend.SetLineColor(0);
 
   hkfactorm.Draw("hist");
-  label->DrawLatex(0.4, 0.5, "k-factor (m)");
+  legend.SetHeader("k-factor (m)");
   fname = mode + "_kfactorm_gen." + fext;
+  legend.Draw();
   gPad->Update();
   gPad->Print(fname.c_str());
 
   hkfactorp.Draw("hist");
-  label->DrawLatex(0.4, 0.5, "k-factor (p)");
-  fname = mode + "_kfactorp_gen." + fext;
+  hMCkfactorp.Draw("hist same");
+  legend.SetHeader("k-factor (p)");
+  fname = mode + "_kfactorp_both." + fext;
+  legend.Draw();
   gPad->Update();
   gPad->Print(fname.c_str());
 
-  hkfactorpm.Draw("hist");
-  label->DrawLatex(0.4, 0.5, "k-factor (m/p)");
-  fname = mode + "_kfactorpm_gen." + fext;
+  hMCkfactorpm.Draw("hist");
+  hkfactorpm.Draw("hist same");
+  legend.SetHeader("k-factor (m/p)");
+  fname = mode + "_kfactorpm_both." + fext;
+  legend.Draw();
   gPad->Update();
   gPad->Print(fname.c_str());
 
@@ -133,6 +174,8 @@ int kfactorp(TTree *outtree, std::string mode, std::string fext)
   hdump.WriteTObject(&hkfactorm, NULL, "WriteDelete");
   hdump.WriteTObject(&hkfactorp, NULL, "WriteDelete");
   hdump.WriteTObject(&hkfactorpm, NULL, "WriteDelete");
+  hdump.WriteTObject(&hMCkfactorp, NULL, "WriteDelete");
+  hdump.WriteTObject(&hMCkfactorpm, NULL, "WriteDelete");
   hdump.Close();
 
   return 0;
